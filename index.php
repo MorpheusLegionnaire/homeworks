3790df2fc22b9b7c9c6782a082a0da2e08c34143
Morpheus <morpheus.legionnaire@gmail.com>
2016-06-22 19:26:23 +0300

    2 measurement

--- index.php
+++ index.php
@@ -5,5 +5,22 @@
  * Date: 22.06.2016
  * Time: 16:26
  */
+echo "<pre>";
+echo "Hello lalka"."<br><br>";
 
-echo "Hello lalka";
+$firstArray['razRaz']='Znachenie1';
+echo"<br><br>";
+print_r($firstArray);
+
+$twoArray['razRaz1']['DvaDva']="MnogomerniyMassiv";
+
+print_r($twoArray);
+
+$ages = array ('1'=>'22', '2'=>'21');
+$city = array ('1'=>'Kiev', '2'=>'Mariupol');
+$neskolko = array($ages, $city);
+
+print_r($neskolko);
+echo "</pre>";
+
+
