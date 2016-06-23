297b58f617c07ceed54b24c924af23c9bdeb78d8
Morpheus <morpheus.legionnaire@gmail.com>
2016-06-23 19:59:30 +0300

    6 (change)

--- chernovik002.php
+++ chernovik002.php
@@ -5,7 +5,14 @@ $b = 34;
 
 if ($a != $b)
 {
-    echo 'It\'s true';
+    if ($a == $b)
+    {
+        echo 'Вложенный if else рабочий';
+    }
+    elseif($a < $b)
+    {
+        echo 'elseif работает';
+    }
 }
 else
 {
