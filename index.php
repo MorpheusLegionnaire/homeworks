3bd388a7d8633cd66fe85dc445f7dfefcdbacaae
Morpheus <morpheus.legionnaire@gmail.com>
2016-06-23 17:59:35 +0300

    4 (change)

--- index.php
+++ index.php
@@ -16,29 +16,28 @@ $twoArray['razRaz1']['DvaDva']="MnogomerniyMassiv";
 
 print_r($twoArray);
 
-$ages = array ('1'=>'22', '2'=>'21');
-$city = array ('1'=>'Kiev', '2'=>'Mariupol');
-$neskolko = array($ages, $city);
+$age = array ('1'=>'22', '2'=>'21');
+$city = array ('1'=>'Kiev', '2'=>'Berlin');
+$neskolko = array($age, $city);
 
 print_r($neskolko);
-echo "</pre>";
+
 
 
 define ('pi', 3.14);
 
 echo pi;
 echo "<br>";
-$myVar = 'hey';
 
-$hey = 'bro';
+$firstVariable = "secondVariable";
+$secondVariable = "the value of the second variable";
 
-$bro = 'Hello, variable';
-
-echo $hey;
-
-echo $$$myVar;
-
+echo $$firstVariable;
 echo "<br>";
+echo $firstVariable;
+
+
+echo "<br><br>";
 
 $x = & $z;
 
@@ -46,3 +45,55 @@ $x = 32;
 $z++;
 
 echo $x;
+echo "<br><br>";
+
+$square000 = "верхний левый угол; правый верхний угол; нижний левый угол; правый нижний угол";
+$square001 = explode("; ", $square000);
+
+print_r($square001);
+
+$arrayNotImplode = array('Name', 'Surname', 'Age', 'Phone', 'email');
+$arrayImplode = implode(", ", $arrayNotImplode);
+echo "<br>";
+echo $arrayImplode;
+
+$abc =& $def;
+
+$abc = 100;
+$def += 15;
+echo $abc;
+$string001 = 2.25;
+
+settype($abc, 'array');
+
+print_r($abc);
+
+settype ($string001, 'integer');
+
+echo $string001;
+echo "<br><br>";
+settype ($arrayNotImplode, 'float');
+
+echo $arrayNotImplode;
+echo "<br><br>";
+
+$newArr['one'] = 'Odin';
+$newArr['two'] = 'Dva';
+$newArr['three'] = 'Tri';
+$newArr['four'] = 'Chetiri';
+$newArr['five'] = 'Pyat\'';
+
+print_r($newArr);
+
+$newNewArr = serialize($newArr);
+
+echo $newNewArr."<br><br>";
+
+$unserArr = unserialize($newNewArr);
+
+print_r($unserArr);
+
+
+
+
+echo "</pre>";
